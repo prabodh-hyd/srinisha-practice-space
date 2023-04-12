d200857f6e3258a4246c46ad6b56e888e2458df8
srinisha-konjerla <128452906+srinisha-konjerla@users.noreply.github.com>
2023-04-12 18:39:44 +0530

    tables

---
+++ javascript/tables.js
@@ -0,0 +1,10 @@
+
+
+function tables(a, b) {
+    for (let i = 0; i <= a; i++) {
+        for (let j = 1; j <= b; j++) {
+            console.log(i + " * " + j + " = " + i * j);
+        }
+    }
+}
+tables(10, 10);
