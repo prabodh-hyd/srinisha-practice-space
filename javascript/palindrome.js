09f8d1b0a80834760356b08e8acd303d4253255e
vikas putcha <developer1@prabodh.in>
2023-07-28 23:56:31 +0530

    fixed folder structure

--- javascript/palindrome.js
+++
@@ -1,8 +0,0 @@
-let s = ["malayalam"]
-function palindrome(str) {
-    return str === str.split('').reverse().join('');
-}
-  console.log(palindrome("s"));
-
-
-
--- javascript/reverse method(array).js
+++
@@ -1,6 +0,0 @@
-let cousins = ["Manisha", "Monika", "Srinisha", "Prashanth", "Durga", "Bhargavi"];
-function family(friends){
-let res = friends.reverse();
-console.log(res);
-}
-family(cousins)
